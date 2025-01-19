78746e98cb4fb3987653aceef8f73a4d5897bb39
Fabián Cabana <fakekian@gmail.com>
2025-01-19 16:57:10 -0500

    Version without wget, not working on docker image

--- week1/ingesting_data_into_pg/ingest_data.py
+++ week1/ingesting_data_into_pg/ingest_data.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
-from time import time 
+
+import argparse
+from time import time
+import urllib.request
 import pandas as pd
 from sqlalchemy import create_engine
-import argparse
-import os
+
 
 def main(params):
     user = params.user
@@ -14,37 +16,76 @@ def main(params):
     db = params.db
     table_name = params.table_name
     url = params.url
-    csv_name = 'output.csv'
-    os.system(f"wget {url} -O {csv_name}")
+
+    print(
+        'Executing with arguments:',
+        'user',user,'\n',
+        'password',password,'\n',
+        'host',host,'\n',
+        'port',port,'\n',
+        'db',db,'\n',
+        'table_name',table_name,'\n',
+        'url',url,'\n',
+    )
+    
+    # the backup files are gzipped, and it's important to keep the correct extension
+    # for pandas to be able to open the file
+    print('downloading file')
+    if url.endswith('.csv.gz'):
+        csv_name = 'output.csv.gz'
+    else:
+        csv_name = 'output.csv'
+
+    try:
+        urllib.request.urlretrieve(url,csv_name)
+    except:
+        raise Exception("Sorry,cant download")
 
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
 
-    df_iter = pd.read_csv(csv_name,iterator=True, chunksize=100000)
-    df  = next(df_iter)
-    df['tpep_dropoff_datetime'] = pd.to_datetime(df.tpep_dropoff_datetime)
-    df['tpep_pickup_datetime'] = pd.to_datetime(df.tpep_pickup_datetime)
+    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
 
-    df.head(n=0).to_sql(name=table_name,con=engine,if_exists='replace')
-    df.to_sql(name=table_name,con=engine,if_exists='append')
+    df = next(df_iter)
 
-    while True:
-        t_start = time()
-        df  = next(df_iter)
-        df['tpep_dropoff_datetime'] = pd.to_datetime(df.tpep_dropoff_datetime)
-        df['tpep_pickup_datetime'] = pd.to_datetime(df.tpep_pickup_datetime)
-        df.to_sql(name=table_name,con=engine,if_exists='append')
-        t_end = time()
-        time_taken = t_end - t_start
-        print(f'inserted another chunk... {time_taken}')
+    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+
+    df.to_sql(name=table_name, con=engine, if_exists='append')
+
+
+    while True: 
+
+        try:
+            t_start = time()
+            
+            df = next(df_iter)
+
+            df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+            df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+            df.to_sql(name=table_name, con=engine, if_exists='append')
+
+            t_end = time()
+
+            print('inserted another chunk, took %.3f second' % (t_end - t_start))
+
+        except StopIteration:
+            print("Finished ingesting data into the postgres database")
+            break
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
-    parser.add_argument('--user', help='user for postgress')
-    parser.add_argument('--password', help='password for postgress')
-    parser.add_argument('--host', help='host for postgress')
-    parser.add_argument('--port', help='port for postgress')
-    parser.add_argument('--db', help='db for postgress')
-    parser.add_argument('--table_name', help='table_name for postgress')
-    parser.add_argument('--url', help='url for postgress')
+
+    parser.add_argument('--user', required=True, help='user name for postgres')
+    parser.add_argument('--password', required=True, help='password for postgres')
+    parser.add_argument('--host', required=True, help='host for postgres')
+    parser.add_argument('--port', required=True, help='port for postgres')
+    parser.add_argument('--db', required=True, help='database name for postgres')
+    parser.add_argument('--table_name', required=True, help='name of the table where we will write the results to')
+    parser.add_argument('--url', required=True, help='url of the csv file')
+
     args = parser.parse_args()
+
     main(args)
---
+++ week1/ingesting_data_into_pg/readme.md
@@ -0,0 +1,16 @@
+# commands to run pg and pgadmin on windows (Had to use winpty)
+winpty docker run -it -e POSTGRES_USER="root" -e POSTGRES_PASSWORD="root" -e POSTGRES_DB="ny_taxi" -v //c/Users/facab/Documents/bootcamp/week1/ingesting_data_into_pg/data/ny_taxi_postgres_data:/var/lib/postgresql/data -p 5432:5432 --network=pgnetwork --name pg-database postgres:13
+
+winpty docker run -it  -e PGADMIN_DEFAULT_EMAIL="admin@admin.com"  -e PGADMIN_DEFAULT_PASSWORD="root"  -p 8080:80  --network=pgnetwork dpage/pgadmin4
+
+# execute ingest_data (I used urllib.request instead of wget.)
+
+URL="https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz"
+
+python ingest_data.py  --user=root --password=root --host=localhost --port=5432 --db=ny_taxi --table_name=yellow_taxi_data --url=${URL}
+
+# Execute dockerized pipeline ingest_data
+
+docker build -t taxi_ingest:v1
+
+docker run taxi:v1 --user=root --password=root --host=localhost --port=5432 --db=ny_taxi --table_name=yellow_taxi_data --url=${URL}
